854e5edec06f21942847d5ac3696f756ebc0bd9a
cheok <cheokml@gmail.com>
2021-04-03 22:25:24 -0700

    Reading The Candle Stick Trading Bible

---
+++ CandleStickTrading.py
@@ -0,0 +1,156 @@
+# The Candle Stick Trading Bible
+# By Munehisa Homma
+
+# What is a candlestick
+"""
+Candles vs Wicks
+Top/Bottom of Candle: Represents Open/CLose Prices
+Top Bottom of Wick: Represents High/Low of candle timeframe
+
+If Candle is:
+    - above open, candlestick = bullish
+    - below open, candlestick = bearish
+
+If Candle body is:
+    - Long: Strong buying/selling pressure
+    - Short: Little buying/selling activity
+
+If wick is:
+    - Long Upper Wick + Short Lower Wick: Buyers bought and bid higher price
+        but sellers drove price back to open price
+    - Short Upper Wick + Long Lower Wick: Sellers sold and forced lower prices
+        but buyers came in and drove prices back to open
+"""
+
+# Engulfing Bar Candlestick Pattern
+"""
+This is when the current candle fully engulfs the previous candle
+    - Must consume more than one previous candle
+    
+Bearish Engulfing Bar
+    - First body is smaller than the second body 
+    - Bearish candle (Close is lower than open) 
+    - Signifies that sellers are in control -> Trend reversal
+        This means that there's a high chance that price will start a downtrend
+        
+Bullish Engulfing Bar
+    - Similar to Bearish Engulfing, first candle is small and second engulfs it
+    - Signifies sellers are no longer in control of the market
+        Buyers are stepping in and taking control of the market
+    - When bullish engulfing bar forms at the end of a downtrend, 
+        reversal is much more powerful -> Represents capitulation bottom
+    - Capitulation means people are giving up and selling the stock
+        so capitulation bottom means people are done selling and are ready to buy again
+"""
+
+# Doji Candlestick Pattern
+"""
+Doji Candlestick occurs when the candle is all wick
+    - This means that the market opened and closed at the same price
+    - Equality and indecision between buyers and sellers
+        No one is in control
+    - Normally means that market is about to reverse or prior trend losing strength
+        ie Uptrend into a doji candlestick normally means it's about to reverse
+    - Could also mean period of resting after big moves
+    - Normally means time to take profits or time to enter
+"""
+
+# Dragonfly Doji Pattern
+"""
+This pattern forms when open + high and close are around the same price 
+    and then there is a long lower wick
+    
+    - This means that there is resistance from buyers and is their attempt to 
+        push the market up
+    - You can use this pattern to identify support and resistance
+    - When this occurs during a downtrend, it is interpreted as a 
+        bullish reversal signal
+"""
+
+# Gravestone Doji Pattern
+"""
+Gravestone Doji is the bearish version of the dragonfly doji
+    Open and close are around the same price but long upper tail
+    
+    - Buyers push price past open price but sellers overwhelm buyers and
+        push it back to open price
+    - Signifies that buyers are losing momentum and market is ready for reversal
+    - For this pattern to be reliable, it must occur near resistance level
+"""
+
+# Morning Star Pattern
+"""
+Bullish Reversal Pattern - Normally happens at the bottom of a downtrend
+    - Holds significant trend reversal signal
+
+Consist of three candlesticks:
+    - First candlestick is bearish: Sellers are still in control
+    - Second candle is smaller -> Sellers are in control but can't push market
+        much lower (can be bullish or bearish candle)
+    - Third candle is bullish that gapped up on open and closed above the midpoint
+        of the first candle
+"""
+
+# Evening Star Pattern
+"""
+Bearish Reversal Pattern - Normally happens at the top of an uptrend
+
+Consist of three candlesticks:
+    - First candlestick is bullish: Buyers are still in control
+    - Second candle is a small candlestick meaning that not many more buyers 
+        This candle normally means price consolidation and indecision
+    - Third candle is a large bearish candle
+    
+"""
+
+# Hammer Pattern
+"""
+Open + High and Close are roughly the same price but long lower wick
+    Similar to dragonfly Doji but the hammer occurs at the bottom of a downtrend
+        - Hammer also closes slightly higher than the open whereas dragonfly
+            doji closes at the same
+        - Dragonfly Doji is normally a stronger reversal pattern
+    Reversal candlestick pattern
+    
+    The long lower wick signifies strong buying power coming in despite
+        sellers trying to push the market lower
+    - Buying pressure > Selling pressure
+    - Results in a trend reversal
+"""
+
+# Shooting Star Pattern
+"""
+Reverse Hammer Pattern:
+    Short candle, long upper wick (wick should be at least twice the length of candle)
+    
+    - Bearish Reversal Signal
+    - Buyers are trying to push market higher
+        Rejected by selling pressure
+    - When this candle forms near resistance level, high probability
+        of downtrend
+"""
+
+# Harami Pattern
+"""
+Considered a reversal and continuation pattern
+
+Consists of two candlesticks
+    - First Candle: Large candle (mother candle)
+    - Second Candle: Smaller candle (baby)
+    - The second candle should close INSIDE the previous one
+    - Bearish reversal signal when it occurs at the top of an uptrend
+    - Bullish reversal signal when it occurs at the bottom of a downtrend
+    - Tells us that the market is consolidating (indecision)
+    - If it occurs during up/downtrend, it can be a continuation pattern
+        which means it's an opportunity to join the trend
+    - If it occurs at the top of an uptrend or bottom of a downtrend
+        it means that it's a trend reversal signal
+"""
+
+# Tweezers Pattern
+
+
+
+
+
+
